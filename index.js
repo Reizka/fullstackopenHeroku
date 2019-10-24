6c40d83e149b68b58acb5374fb92413375798c6a
Renny Lindberg <reizka.deepone@gmail.com>
2019-10-24 12:05:14 +0200

    changes

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static('build'));
 /*morgan.token('response', function(req, res) {
   return JSON.stringify(req.body);
 });
